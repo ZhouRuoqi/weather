8d20af1910feb02b1c38f06a8868b8eb53150b92
ZhouRuoqi <1195266504@qq.com>
2020-04-24 00:24:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private TextView tv_tianqi;
+    private TextView tv_wendu;
+    private TextView tv_fengli;
+    private String API = "https://free-api.heweather.com/s6/weather/now?key=026ed4dfee62444c9335e82caa9d3a21&location=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.main_editView);
+        button = findViewById(R.id.main_button);
+        tv_tianqi = findViewById(R.id.main_tianqi);
+        tv_fengli = findViewById(R.id.main_fengli);
+        tv_wendu = findViewById(R.id.main_wendu);
+//        try {
+//            Class.forName("android.os.AsyncTask");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String city = editText.getText().toString();
+                new MyWeather().execute(API + city);
+
+            }
+        });
+    }
+
+    class MyWeather extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuffer stringBuffer = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = null;
+                if (httpURLConnection.getResponseCode() == 200) {
+                    inputStream = httpURLConnection.getInputStream();
+                    //检测网络异常
+                } else {
+                    return "11";
+                }
+                InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                stringBuffer = new StringBuffer();
+                String timp = null;
+                while ((timp = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(timp);
+                }
+                inputStream.close();
+                reader.close();
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return stringBuffer.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s.equals("11")) {
+                Toast.makeText(MainActivity.this, "网络异常", Toast.LENGTH_SHORT).show();
+            }
+            try {
+                JSONObject object = new JSONObject(s);
+                JSONObject object1 = object.getJSONArray("HeWeather6").getJSONObject(0);
+                JSONObject basic = object1.getJSONObject("basic");
+                JSONObject now = object1.getJSONObject("now");
+                String city = basic.getString("location");
+                String tianqi = now.getString("cond_txt");
+                String wendu = now.getString("tmp");
+                String fengli = now.getString("wind_dir");
+                String qiangdu = now.getString("wind_sc");
+                tv_tianqi.setText( tianqi);
+                tv_wendu.setText(wendu + "℃");
+                tv_fengli.setText(fengli + qiangdu + "级");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
+
